49602641c98110178b0b05f525da6750d0e5d34e
Shueb Dalvi <admin@shueb.in>
2017-12-17 22:16:19 +0530

    initial project setup

---
+++ src/main/java/in/shueb/todo/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package in.shueb.todo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication().withUser("user").password("pass").roles("USER");
+	}
+	
+}
